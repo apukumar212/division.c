385facacb5f36b3cab3a6ed61cd9c119960318f4
Apu kumar Giri <87590236+apukumnar212@users.noreply.github.com>
2022-07-14 14:49:15 +0530

    Add files via upload

---
+++ divisin.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+int a=1.2;
+float b=12;
+system("cls");
+  printf("a=%d\n",a);
+  printf("b=%f\n",b);
+  printf("%d\n",5/2);
+  printf("%f\n",5.0/2);
+  printf("%f\n",5/2.0);
+  printf("%f\n",5.0/2.0);
+  printf("%f\n",(float)5/2);
+  printf("%f\n",(float)(5/2));
+  getch();
+  }
+  
